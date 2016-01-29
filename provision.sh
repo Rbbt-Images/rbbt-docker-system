db380b8d7c5593bd82bb86b643a5c141018e7134
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2016-01-29 17:31:08 +0100

    Improved provision.sh layout

--- provision.sh
+++ provision.sh
@@ -1,9 +1,11 @@
-cat "$0"
-echo "Running provisioning"
-echo
+#!/bin/bash -x
 
-# BASE SYSTEM
+echo "RUNNING PROVISION"
+echo
+echo "CMD: git/rbbt-image/bin/build_rbbt_provision_sh.rb -sg -su -sb"
+
 echo "1. Provisioning base system"
+echo
 #!/bin/bash -x
 
 # INSTALL
@@ -32,13 +34,12 @@ rm /usr/lib/R/bin/Rserve
 ln -s /usr/lib/R/site-library/Rserve/libs/Rserve /usr/lib/R/bin/Rserve
 
 
-
-# BASE SYSTEM
 echo "2. Setting up ruby"
+echo
 #!/bin/bash -x
 
-# Ruby gems and Rbbt
-# -------------------------
+# RUBY INSTALL
+# ============
 
 cd /tmp
 wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.5.tar.gz 
@@ -52,109 +53,23 @@ grep "#Ruby2" /etc/profile || echo 'export PATH="/usr/local/bin:$PATH" #Ruby2' >
 
 
 
-# BASE SYSTEM
-echo "2. Setting up gems"
+echo "3. Setting up gems"
+echo
+echo SKIPPED
 
+echo "4. Configuring user"
+echo
+echo SKIPPED
 
-exit
+echo "5. Bootstrapping workflows as 'rbbt'"
+echo
+echo SKIPPED
 
-####################
-# USER CONFIGURATION
-
-if [[ 'rbbt' == 'root' ]] ; then
-  home_dir='/root'
-else
-  useradd -ms /bin/bash rbbt
-  home_dir='/home/rbbt'
-fi
-
-user_script=$home_dir/.rbbt/bin/provision
-mkdir -p $(dirname $user_script)
-chown -R rbbt /home/rbbt/.rbbt/
-cat > $user_script <<'EUSER'
-
-. /etc/profile
-
-echo "2.1. Custom variables"
-export RBBT_LOG="0"
-export BOOTSTRAP_WORKFLOWS="Enrichment Translation Sequence MutationEnrichment"
-export REMOTE_RESOURCES="KEGG"
-
-echo "2.2. Default variables"
-#!/bin/bash -x
-
-test -z ${RBBT_SERVER+x}           && RBBT_SERVER=http://rbbt.bioinfo.cnio.es/ 
-test -z ${RBBT_FILE_SERVER+x}      && RBBT_FILE_SERVER="$RBBT_SERVER"
-test -z ${RBBT_WORKFLOW_SERVER+x}  && RBBT_WORKFLOW_SERVER="$RBBT_SERVER"
-
-test -z ${REMOTE_RESOURCES+x}  && REMOTE_RESOURCES="Organism ICGC COSMIC KEGG InterPro"
-test -z ${REMOTE_WORFLOWS+x}   && REMOTE_WORFLOWS=""
-
-test -z ${RBBT_WORKFLOW_AUTOINSTALL+x}  && RBBT_WORKFLOW_AUTOINSTALL="true"
-
-test -z ${WORKFLOWS+x}  && WORKFLOWS=""
-
-test -z ${BOOTSTRAP_WORKFLOWS+x}  && BOOTSTRAP_WORKFLOWS=""
-test -z ${BOOTSTRAP_CPUS+x}       && BOOTSTRAP_CPUS="2"
-
-test -z ${RBBT_LOG+x}  && RBBT_LOG="LOW"
-
-
-
-echo "2.3. Configuring rbbt"
-#!/bin/bash -x
-
-# GENERAL
-# -------
-
-# File servers: to speed up the production of some resources
-for resource in $REMOTE_RESOURCES; do
-    echo "Adding remote file server: $resource -- $RBBT_FILE_SERVER"
-    rbbt file_server add $resource $RBBT_FILE_SERVER
-done
-
-# Remote workflows: avoid costly cache generation
-for workflow in $REMOTE_WORKFLOWS; do
-    echo "Adding remote workflow: $workflow -- $RBBT_WORKFLOW_SERVER"
-    rbbt workflow remote add $workflow $RBBT_WORKFLOW_SERVER
-done
-
-
-exit
-
-echo "2.4. Bootstrap system"
-#!/bin/bash -x
-
-# APP
-# ---
-
-for workflow in $WORKFLOWS; do
-    rbbt workflow install $workflow 
-done
-
-export RBBT_WORKFLOW_AUTOINSTALL
-export RBBT_LOG
-
-for workflow in $BOOTSTRAP_WORKFLOWS; do
-    echo "Bootstrapping $workflow on $BOOTSTRAP_CPUS CPUs"
-    rbbt workflow cmd $workflow bootstrap $BOOTSTRAP_CPUS
-done
-
-
-EUSER
-####################
-echo "2. Running user configuration as 'rbbt'"
-chown rbbt $user_script;
-su -l -c "bash $user_script" rbbt
-
-# CLEAN-UP
+# CODA
+# ====
 
 RUN apt-get clean
 RUN rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-# DONE
 echo
 echo "Installation done."
-
-#--------------------------------------------------------
-
