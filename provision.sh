c94b710e673c00481c4abe8404da25df6cd28d2d
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2017-02-13 16:03:49 +0100

    nil

--- provision.sh
+++ provision.sh
@@ -55,8 +55,8 @@ grep -v R_HOME /etc/profile > profile.tmp
 echo >> profile.tmp
 echo "# For RSRuby gem " >> profile.tmp
 echo "export R_HOME='/usr/local/lib/R'" >> profile.tmp
-echo "export LD_LIBRARY_PATH=\"$LD_LIBRARY_PATH:$R_HOME/lib\"" >> profile.tmp
-echo "export LD_RUN_PATH=\"$LD_RUN_PATH:$R_HOME/lib\"" >> profile.tmp
+echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:\$R_HOME/lib\"" >> profile.tmp
+echo "export LD_RUN_PATH=\"\$LD_RUN_PATH:\$R_HOME/lib\"" >> profile.tmp
 mv profile.tmp /etc/profile
 . /etc/profile
 
